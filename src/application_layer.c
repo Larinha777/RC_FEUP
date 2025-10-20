010dab95810fe284f41082a4d884e93c5d89e283
Teresa Santos <teresafreitassantos@gmail.com>
2025-10-20 17:09:28 +0100

    updated

--- src/application_layer.c
+++ src/application_layer.c
@@ -1,11 +1,72 @@
 // Application layer protocol implementation
 
 #include "application_layer.h"
-
 #include <stdio.h>
+#include "link_layer.h"
+#include <string.h>
+
+
+//used by Rx
+void createFile(/**/){} 
+void writeFile(){}
+void parsePck(unsigned char *packet){}
+void extractDataPck(unsigned char *packet){}
+
+//used by Tx
+void openFile(const char *filename){} 
+void readFragFile(/**/){} 
+void buildCtrlPck(/**/){} 
+void buildDataPck(/**/){} 
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
-    // TODO: Implement this function
+    
+    LinkLayer connectionParameters;
+    strcpy(connectionParameters.serialPort, serialPort);
+    if (strcmp(role, "tx")){
+        connectionParameters.role = LlTx;
+    } else {
+        connectionParameters.role = LlRx;
+    }
+    connectionParameters.baudRate = baudRate;
+    connectionParameters.nRetransmissions = nTries;
+    connectionParameters.timeout = timeout;
+    
+    
+    // open connection 
+    // done by both Tx and Rx
+    llopen(connectionParameters);
+
+    
+    if (connectionParameters.role == LlRx){ //Rx
+        
+        unsigned char packet[MAX_PAYLOAD_SIZE];
+        llread(packet); // gets start packet
+        createFile(/**/);
+        while (TRUE /*data != endpacket*/){ // maybe state machine
+            llread(packet);
+            parsePck(packet); // q faz isto??
+            extractDataPck(packet);
+            writeFile(/**/);
+        }
+        
+        llclose(); 
+
+    } else { //Tx
+        openFile(filename); 
+        buildCtrlPck(/**/); //start packet
+        while (TRUE /*ha cenas no ficheiro*/){
+            readFragFile(/**/);
+            unsigned char *data_buf;
+            int data_bufSize;
+            buildDataPck(/**/); 
+            llwrite(data_buf, data_bufSize);
+        }
+
+        buildCtrlPck(/**/); //end packet
+
+        llclose();
+    }
+
 }
--- src/link_layer.c
+++ src/link_layer.c
@@ -2,16 +2,149 @@
 
 #include "link_layer.h"
 #include "serial_port.h"
-
+#include "flag_receiver.h"
+#include "alarm.h"
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
+
+LinkLayer connectParam;
+int inf_frame_num = 0;
+
+int receive_packet(unsigned char *buf, setMessageState *state){
+    volatile int STOP = FALSE;
+    Packet packet;
+    packet.cur_state = START;
+    int bytes = 1;
+    while (connectParam.role == LlRx || (connectParam.role == LlTx && bytes == 1 ) )
+    {
+        unsigned char byte_rcv;
+        bytes = readByteSerialPort(&byte_rcv); //this might return -1, maybe check for that
+        if (bytes == -1){
+            return -1;
+        }
+
+        updateCurrentState(&packet, byte_rcv);
+
+        /*
+        printf("Byte received: %c\n", byte);
+        printf("var = 0x%02X\n", byte);
+        */
+        
+        if (packet.cur_state == SET_RCV || packet.cur_state == UA_RCV || packet.cur_state == DISC_RCV
+            || packet.cur_state == REJ0_S || packet.cur_state == REJ1_S )
+        {
+            STOP = TRUE;
+        } else if (packet.cur_state == RR0_S || packet.cur_state == RR1_S){
+            buf = packet.data;
+            STOP = TRUE;
+        } 
+    }
+    *state = packet.cur_state;
+    return 0;
+}
+
+
+int send_packet(const unsigned char *send_buf, int bufSize, setMessageState *ret_state){ 
+    volatile int STOP = FALSE;
+    
+    struct sigaction act = {0};
+    act.sa_handler = &alarmHandler;
+    if (sigaction(SIGALRM, &act, NULL) == -1)
+    {
+        perror("sigaction");
+        return -1;
+    }
+
+    int bytes = writeBytesSerialPort(send_buf, bufSize); // check -1
+    sleep(1);
+
+    while (STOP == FALSE)
+    {
+        unsigned char receive_buf;
+        setMessageState cur_state;
+        receive_packet(&receive_buf, &cur_state);
+        
+        if (cur_state == UA_RCV || cur_state == DISC_RCV || (cur_state == RR0_S && inf_frame_num == 1)  || (cur_state == RR1_S && inf_frame_num == 0) ){
+            *ret_state = cur_state;
+            alarm(0);
+            STOP = TRUE;
+        }
+
+        if (alarmCount < connectParam.nRetransmissions)
+        {
+            if ((cur_state == REJ0_S && inf_frame_num == 0) || (cur_state == REJ1_S && inf_frame_num == 1)){
+                alarm(connectParam.timeout); 
+                alarmCount++;
+                alarmEnabled = TRUE;
+                int bytes = writeBytesSerialPort(send_buf, bufSize); // check -1
+                sleep(1);
+                continue;
+            }
+
+            if (alarmEnabled == FALSE)
+            {
+                alarm(connectParam.timeout); 
+                alarmEnabled = TRUE;
+                
+                int bytes = writeBytesSerialPort(send_buf, bufSize); // check -1
+                sleep(1);
+            }
+        } 
+        else 
+        {
+            return -1;
+        }
+    }
+    return 0;
+}
+
 
 ////////////////////////////////////////////////
 // LLOPEN
 ////////////////////////////////////////////////
 int llopen(LinkLayer connectionParameters)
 {
-    // TODO: Implement this function
+    if (openSerialPort(connectionParameters.serialPort, connectionParameters.baudRate) < 0)
+    {
+        perror("openSerialPort");
+        return -1;
+    }
+
+    connectParam = connectionParameters;
+
+    setMessageState state;
+    
+    if (connectionParameters.role == LlTx) { //Transmiter  
+        
+        unsigned char buf[MAX_DATA_SIZE] = {0}; // Duvidassss!!!!!!!!!!
+        buf[0] = FLAG;
+        buf[1] = ADDRESS_BY_SENDER;
+        buf[2] = CONTROL_SET;
+        buf[3] = buf[1] ^ buf[2];
+        buf[4] = FLAG;
+        
+        setMessageState ret_state;
+        if (send_packet(buf, 5, &ret_state) != 0){// Duvidassss!!!!!!!!!! Necessario dar check de ua?
+            return -1;
+        }
+
+    } else { //Receiver
+        unsigned char buf[MAX_DATA_SIZE] = {0}; // Duvidassss!!!!!!!!!!
+        
+        if (receive_packet(buf, &state) != 0 || state != SET_RCV){
+            return -1;
+        } 
+        
+        buf[0] = FLAG;
+        buf[1] = ADDRESS_BY_SENDER;
+        buf[2] = CONTROL_UA;
+        buf[3] = buf[1] ^ buf[2];
+        buf[4] = FLAG;
+
+        writeBytesSerialPort(buf, 5); // check -1
+        sleep(1);
+
+    }
 
     return 0;
 }
@@ -19,19 +152,151 @@ int llopen(LinkLayer connectionParameters)
 ////////////////////////////////////////////////
 // LLWRITE
 ////////////////////////////////////////////////
-int llwrite(const unsigned char *buf, int bufSize)
-{
-    // TODO: Implement this function
+int llwrite(const unsigned char *data_buf, int data_bufSize)
+{ 
+    unsigned char packet_buf[MAX_PACKET_SIZE] = {0};
 
+    packet_buf[0] = FLAG;
+    packet_buf[1] = ADDRESS_BY_SENDER;
+    
+    if (inf_frame_num == 0){
+        packet_buf[2] = I0;
+    } else {
+        packet_buf[2] = I1;
+    }
+
+    packet_buf[3] = packet_buf[1] ^ packet_buf[2];
+
+    unsigned char bcc2 = 0;
+    int i = 0;
+    while (i < data_bufSize){
+        bcc2 ^= data_buf[i];
+        
+        if (data_buf[i] == ESC || data_buf[i] == FLAG){
+            packet_buf[i+4] = ESC;
+            i++;
+            packet_buf[i+4] = data_buf[i] ^ XOR_OP; 
+            data_bufSize++;
+        } else {
+            packet_buf[i+4] = data_buf[i];
+        }
+        i++;
+    }
+
+    if (bcc2 == ESC || bcc2 == FLAG){
+        packet_buf[data_bufSize + 4] = ESC;
+        packet_buf[data_bufSize + 5] = bcc2 ^ XOR_OP; 
+        packet_buf[data_bufSize + 6] = FLAG;
+    } else {
+        packet_buf[data_bufSize + 4] = bcc2;
+        packet_buf[data_bufSize + 5] = FLAG;
+    }
+
+    setMessageState ret_state = START;
+
+    while (!(((ret_state == RR1_S) && (inf_frame_num == 0)) || ((ret_state == RR0_S) && (inf_frame_num == 1)))){
+        send_packet(packet_buf, MAX_PACKET_SIZE, &ret_state);
+    }
+
+    inf_frame_num ^= 1;
     return 0;
 }
 
 ////////////////////////////////////////////////
 // LLREAD
 ////////////////////////////////////////////////
-int llread(unsigned char *packet)
+int llread(unsigned char *packet) //
 {
-    // TODO: Implement this function
+    setMessageState state;
+    unsigned char buf[MAX_DATA_SIZE] = {0};
+    
+    if (receive_packet(buf, &state) != 0){
+        return -1;
+    } 
+
+
+    switch (state)
+    {
+    case RR0_S:
+        // if inf_fram_num == 0 
+        // repeated packet was sent 
+        // wanted 0 received 1
+
+        // correct packet was sent
+        // wanted 1 received 1
+        if (inf_frame_num == 1)
+        { 
+            packet = buf;
+        }
+
+        buf[2]=RR0;
+        buf[3]=ADDRESS_BY_SENDER ^ RR0;
+        break;
+
+    case RR1_S:
+        // correct packet was sent 
+        // wanted 0 received 0
+        if (inf_frame_num == 0){
+            packet = buf;
+        }
+
+        // if inf_fram_num == 1 
+        // repeated packet was sent 
+        // wanted 1 received 0
+
+        buf[2]=RR1;
+        buf[3]=ADDRESS_BY_SENDER ^ RR1;
+        break;
+
+    case REJ0_S:
+
+        // faulty packet was sent 
+        // wanted 0 received incorrect 0
+        if (inf_frame_num == 0){
+            buf[2]=REJ0;
+            buf[3]=ADDRESS_BY_SENDER ^ REJ0;
+        } 
+
+        // if inf_fram_num == 1 
+        // faulty packet sent was a duplicate 
+        // wanted 1 received 0 (already has correct 0)
+        else 
+        {
+            buf[2]=RR1;
+            buf[3]=ADDRESS_BY_SENDER ^ RR1;
+        }
+        break;    
+
+    case REJ1_S:
+        // if inf_fram_num == 0
+        // faulty packet sent was a duplicate 
+        // wanted 0 received 1 (already has correct 1)
+        if (inf_frame_num == 0){
+            buf[2]=RR0;
+            buf[3]=ADDRESS_BY_SENDER ^ RR0;            
+        } 
+
+        // if inf_fram_num == 1
+        // faulty packet was sent
+        // wanted 1 received faulty 1
+        else 
+        {
+            buf[2]=REJ1;
+            buf[3]=ADDRESS_BY_SENDER ^ REJ1;
+        }
+
+        break;    
+
+    default:
+        return -1;
+    }
+
+    buf[0]=FLAG;
+    buf[1]=ADDRESS_BY_SENDER;
+    buf[4]=FLAG;
+
+    writeBytesSerialPort(buf, 5); // check -1
+    sleep(1);
 
     return 0;
 }
@@ -40,8 +305,31 @@ int llread(unsigned char *packet)
 // LLCLOSE
 ////////////////////////////////////////////////
 int llclose()
-{
+{ // to do
+    unsigned char buf[MAX_DATA_SIZE] = {0}; // Duvidassss!!!!!!!!!!
+    buf[0] = FLAG;
+    buf[1] = ADDRESS_BY_SENDER;
+    buf[2] = DISC;
+    buf[3] = buf[1] ^ buf[2];
+    buf[4] = FLAG;
+    
+    setMessageState ret_state;
+    if (send_packet(buf, 5, &ret_state) != 0){
+        return -1;
+    }
+    buf[2] = CONTROL_UA;
+    buf[3] = buf[1] ^ buf[2];
+    writeBytesSerialPort(buf, 5); // check -1
+    
+    if (closeSerialPort() < 0)
+    {
+        perror("closeSerialPort");
+        return -1;
+    }
+
     // TODO: Implement this function
 
     return 0;
 }
+
+
