9a6f138cd10c9c9f8890b162f2cad794f7f7982e
Larinha777 <lsilvamoreira7@gmail.com>
2025-12-01 16:03:32 +0000

    Added parse_URL

--- proj2/download.c
+++ proj2/download.c
@@ -16,25 +16,47 @@ typedef struct {
     char** ip;
 } Url_data;
 
-
+/* If the url is a valid one, it works, if not, it doesnt*/
 int parse_URL(char* url_str, Url_data* url_struct ){
-    
-    printf("parse_URL not implemented");
-    return 1;
+   
+    /* url does not start with ftp */
+    if (strncmp("ftp", url_str, 3)){ 
+        printf("Cannot handle cases where the url does not start with ftp\n");
+        return 1;
+    }
+
+    //printf("full str: %s\n",url_str);
+
+    char* noFTP = strstr(url_str, "//") + 2;
+    //printf("noFTP str: %s\n",noFTP);
+
+    char* part1 = strtok(noFTP, "@");
+    char* part2 = strtok(NULL, "");
+
+    if (part2 == NULL){ //no login
+        url_struct->user = "";
+        url_struct->password = "";
+        url_struct->host = strtok(part1, "/");
+        url_struct->url_path = strtok(NULL, "");
+    } else {
+        url_struct->user = strtok(part1, ":");
+        url_struct->password = strtok(NULL, "");
+        url_struct->host = strtok(part2, "/");
+        url_struct->url_path = strtok(NULL, "");
+    }
+    return 0;
 }
 
 int check_valid_url( Url_data* url_struct ){
     //se erro dar print
-    printf("check_valid_url not implemented");
+    printf("Function check_valid_url not implemented\n");
     return 1;
 }
 
 int getip( Url_data* url_struct ){
-    printf("getip not implemented");
+    printf("Function getip not implemented\n");
     return 1;
 }
-
-
 
 int main(int argc, char **argv) {
     // ftp://demo:password@test.rebex.net/readme.txt
@@ -52,13 +74,19 @@ int main(int argc, char **argv) {
     if (parse_URL( argv[1], &url) == 1){
         return 1;
     }
+
+    // printf("user: %s\n",url.user);
+    // printf("password: %s\n",url.password);
+    // printf("host: %s\n",url.host);
+    // printf("url_path: %s\n",url.url_path);  
+    
     
     if (check_valid_url(&url) == 1){
         return 1;
     }
     
     if (getip(&url) == 1){ // buscar ip e companhia
-        printf("Ip not found");
+        printf("Ip not found\n");
         return 1;
     }
 
